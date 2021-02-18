2227fa1ae525b648a00c21644a75911a645aed79
sunshodan <sunshodan@gmail.com>
2021-02-18 17:06:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//JavaScript Console Api
+//Way to print in JS
+// console.log("Hello World");
+// FirstContainer.innerHTML="Hello Sun";
+// console.warn("Warning");
+// document.write("Wtf");
+// alert("Hi");
+// let st="I am a good boy";
+// st=st.replace("good","bad");
+// console.log(st);
+let x=document.getElementById("btn");
+// console.log(x);
+x.style.backgroundColor="green";
+// let val=document.getElementById("hi");
+// val.classList.add("important");
+console.log(val);
